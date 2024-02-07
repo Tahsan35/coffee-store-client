3be0ae0e1008b6bb7f83098b61e838be07f6b932
Tahsan <tahsanknmas14@gmail.com>
2024-02-07 16:31:12 +0600

    added the basic set up of the components

---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,9 @@
+const CoffeeCard = () => {
+  return (
+    <div>
+      <h2>this is coffee card</h2>
+    </div>
+  );
+};
+
+export default CoffeeCard;
---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,9 @@
+const UpdateCoffee = () => {
+  return (
+    <div>
+      <h3>update coffee</h3>
+    </div>
+  );
+};
+
+export default UpdateCoffee;
